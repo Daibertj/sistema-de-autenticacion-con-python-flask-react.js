ae36884d5d44b13570d8d4d9d3661c0d73c3b55f
Daibert Ramirez <daibertjrg@gmail.com>
2023-08-10 23:11:11 +0000

    correccion de registro

--- src/front/js/pages/Formulario.jsx
+++ src/front/js/pages/Formulario.jsx
@@ -21,7 +21,7 @@ const Formulario = () => {
 
     try {
       const response = await actions.registerUser(user);
-      if (response === 201 || 200) {
+      if (response == 200) {
         console.log("Registro exitoso");
         navigate("/");
       }
